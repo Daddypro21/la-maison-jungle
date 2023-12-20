86cc26c05c2e6d21b83afe4d44cd4b7a69aa86ab
Daddypro21 <sndemapro@gmail.com>
2023-12-20 11:45:37 +0100

    second commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,13 @@
 
 import '../style/Banner.css'
+import logo from '../assets/10259340_4401280.jpg'
 
 function Banner() {
     return (
-    <div className="lmj-banner"><h1>La maison jungle</h1></div>)
+    <div className="lmj-banner">
+        <img src={ logo } alt="la maison jungle" className="lmj-logo"/>
+        <h1>La maison jungle @</h1>
+        </div>)
 }
 
 export default Banner
