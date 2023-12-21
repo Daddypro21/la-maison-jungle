dcef3272d82190ef082b4b69cda48d1cd25f7d22
Daddypro21 <sndemapro@gmail.com>
2023-12-21 16:05:56 +0100

    deuxieme commit ajout des composants PlantItem,CareScale et du style

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
-
 import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../style/ShoppingList.css'
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -12,13 +13,18 @@ function ShoppingList() {
 		<div>
 			<ul>
 				{categories.map((cat) => (
-					<li key={cat} >{cat}</li>
+					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
-				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}  {plant.isBestSale ? <span>🔥</span> : <span>👎</span>}</li>
-                   
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
